65486ffdc1be96efa46570dbc281ab46f46ca5f2
VoDucHuy <voduchuy542@gmail.com>
2023-05-29 08:14:08 +0700

    Initialize Project

---
+++ src/main/java/backend/qlgiaibongda/QlgiaibongdaApplication.java
@@ -0,0 +1,13 @@
+package backend.qlgiaibongda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlgiaibongdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlgiaibongdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/qlgiaibongda/QlgiaibongdaApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.qlgiaibongda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlgiaibongdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
