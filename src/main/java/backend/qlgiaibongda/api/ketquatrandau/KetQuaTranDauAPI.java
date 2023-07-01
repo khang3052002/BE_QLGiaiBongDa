649b9f41fb510f8a1066d4bae14654ad3acf8336
20120506 <tranduyheo123@gmail.com>
2023-07-01 23:56:46 +0700

    update ketQuaTranDau

--- src/main/java/backend/qlgiaibongda/api/ketquatrandau/KetQuaTranDauAPI.java
+++ src/main/java/backend/qlgiaibongda/api/ketquatrandau/KetQuaTranDauAPI.java
@@ -4,6 +4,8 @@ import backend.qlgiaibongda.api.input.ListIdMatchResult;
 import backend.qlgiaibongda.api.input.ListMatchResultInput;
 import backend.qlgiaibongda.api.input.MatchResultInput;
 import backend.qlgiaibongda.dto.ResponeObject;
+import backend.qlgiaibongda.entity.TranDauEntity;
+import backend.qlgiaibongda.repository.TranDauRepository;
 import backend.qlgiaibongda.service.IBangXepHangService;
 import backend.qlgiaibongda.service.IKetQuaTranDauService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,8 @@ public class KetQuaTranDauAPI {
     private IKetQuaTranDauService ketQuaTranDauService;
     @Autowired
     private IBangXepHangService bangXepHangService;
+    @Autowired
+    private TranDauRepository tranDauRepository;
     @GetMapping("/{id}")
     public ResponseEntity<ResponeObject> getMatchResult(@PathVariable("id") Long id){
         return ketQuaTranDauService.getOne(id);
@@ -32,15 +36,24 @@ public class KetQuaTranDauAPI {
                     .body(new ResponeObject("FAIL", "Invalid Info", ""));
         }
         else {
-            Integer flagHoa0_0 = listMatchResultInput.getFlag_Hoa_0_banthang();
-            ResponseEntity<ResponeObject> respone = ketQuaTranDauService.updateMatchResult(listMatchResultInput,id_trandau,flagHoa0_0);
-            Boolean checkUpdateRank = bangXepHangService.UpdateRanking(id_trandau);
-            if(checkUpdateRank == false)
+            TranDauEntity tranDauEntity = tranDauRepository.findById(id_trandau).orElse(null);
+            if(tranDauEntity.getLichThiDau().getMuaGiai().getTrangThai() == 1)
             {
-                return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
-                        .body(new ResponeObject("FAIL", "Cập nhật bảng xếp hạng thất bại", ""));
+                Integer flagHoa0_0 = listMatchResultInput.getFlag_Hoa_0_banthang();
+                ResponseEntity<ResponeObject> respone = ketQuaTranDauService.updateMatchResult(listMatchResultInput,id_trandau,flagHoa0_0);
+                Boolean checkUpdateRank = bangXepHangService.UpdateRanking(id_trandau);
+                if(checkUpdateRank == false)
+                {
+                    return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
+                            .body(new ResponeObject("FAIL", "Cập nhật bảng xếp hạng thất bại", ""));
+                }
+                return respone;
             }
-            return respone;
+            else{
+                return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
+                        .body(new ResponeObject("FAIL", "Mùa giải chưa bắt đầu!", ""));
+            }
+
         }
 
     }
